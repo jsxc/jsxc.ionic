05f2e8cac8c70e709ed431232bc0bb8efc8708a6
Andrei Sacuiu <andrei.sacuiu@uni-konstanz.de>
2017-12-14 15:22:59 +0100

    Revert "rebase"
    This reverts commit cb545cee5de7b43cecdd98e1b631f6b15a880d5c.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JSXC } from "jsxc";
 
 import { NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
@@ -18,22 +19,22 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
-    // let nv = this.navCtrl;
-    // document.addEventListener('ionic.disconnected', function(e){
-    //   let login_error = new CustomEvent('login.fail',{detail: "unkwown"});
-    //   document.dispatchEvent(login_error);
-    //   nv.push(LoginFailPage); 
-    // });
-    // this.storage.get('bosh').then((bosh) => {
-    //   this.storage.get('username').then((username) => {
-    //     this.storage.get('password').then((password) => {
-    //       let login_details = {bosh:bosh,
-    //                            username:username,
-    //                            password:password}
-    //       let login_event = new CustomEvent('login.trigger',{detail: login_details});
-    //       document.dispatchEvent(login_event);
-    //     }); 
-    //   }); 
-    // });    
+    let nv = this.navCtrl;
+    document.addEventListener('ionic.disconnected', function(e){
+      let login_error = new CustomEvent('login.fail',{detail: "unkwown"});
+      document.dispatchEvent(login_error);
+      nv.push(LoginFailPage); 
+    });
+    this.storage.get('bosh').then((bosh) => {
+      this.storage.get('username').then((username) => {
+        this.storage.get('password').then((password) => {
+          let login_details = {bosh:bosh,
+                               username:username,
+                               password:password}
+          let login_event = new CustomEvent('login.trigger',{detail: login_details});
+          document.dispatchEvent(login_event);
+        }); 
+      }); 
+    });    
   }
 }
